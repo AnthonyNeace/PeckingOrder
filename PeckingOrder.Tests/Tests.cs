cbec1a40b242484a2814a6a4d6c7d3b642dce1f8
Anthony Neace <hyperanthony@gmail.com>
2018-01-07 02:24:05 -0500

    Initial commit of library
    Includes initial sorter and tests.

---
+++ PeckingOrder.Tests/Tests.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using PeckingOrder;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace PeckingOrder.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase("City", false)]
+        [TestCase("State", true)]
+        [TestCase("Country", false)]
+        [TestCase("Default", true)]
+        public void Given_ValidSingleItem_When_ResolvingSettings_Then_ReturnSetting(string key, bool value)
+        {
+            Sorter<string, int> target = buildTarget();
+
+            var isNewYearsAHoliday = new List<(string mode, bool value)>()
+            {
+                (key,value)
+            };
+
+            var actual = target.Resolve(isNewYearsAHoliday);
+
+            Assert.That(actual == value);
+        }
+
+        [TestCase("Potatoes", false)]
+        public void Given_InvalidSingleItem_When_ResolvingSettings_Then_ThrowArgumentOutOfRangeException(string key, bool value)
+        {
+            Sorter<string, int> target = buildTarget();
+
+            var isNewYearsAHoliday = new List<(string mode, bool value)>()
+            {
+                (key,value)
+            };
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => target.Resolve(isNewYearsAHoliday));
+        }
+
+        [Test]
+        public void Given_ValidCollection_When_ResolvingSettings_Then_ReturnSetting()
+        {
+            Sorter<string, int> target = buildTarget();
+
+            var locationNames = new List<(string mode, string value)>()
+            {
+                ("Country","United States of America"),
+                ("City","Las Vegas"),
+                ("State","Nevada")
+            };
+
+            var actual = target.Resolve(locationNames);
+
+            Assert.That(actual == "Las Vegas");
+        }
+
+        [Test]
+        public void Given_CollectionWithInvalidEntry_When_ResolvingSettings_Then_ThrowArgumentOutOfRangeException()
+        {
+            Sorter<string, int> target = buildTarget();
+
+            var locationNames = new List<(string mode, string value)>()
+            {
+                ("Country","United States of America"),
+                ("City","Las Vegas"),
+                ("Street","123 Main Street")
+            };
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => target.Resolve(locationNames));
+        }
+
+        [Test]
+        public void Given_NullCollection_When_ResolvingSettings_Then_ThrowArgumentNullException()
+        {
+            Sorter<string, int> target = buildTarget();
+
+            Assert.Throws<ArgumentNullException>(() => target.Resolve<(string, int)>(null));
+        }
+
+        [Test]
+        public void Given_EmptyCollection_When_ResolvingSettings_Then_ThrowArgumentException()
+        {
+            Sorter<string, int> target = buildTarget();
+
+            var locationNames = new List<(string mode, string value)>()
+            {
+
+            };
+
+            Assert.Throws<ArgumentException>(() => target.Resolve(locationNames));
+        }
+
+        [Test]
+        public void Given_NullOrderAndValidCollection_When_ResolvingSettings_Then_ThrowInvalidOperationException()
+        {
+            Sorter<string, int> target = new Sorter<string, int>();
+
+            target.Order = null;
+
+            var locationNames = new List<(string mode, string value)>()
+            {
+                ("Country","United States of America"),
+                ("City","Las Vegas"),
+                ("State","Nevada")
+            };
+
+            Assert.Throws<InvalidOperationException>(() => target.Resolve<(string, int)>(null));
+        }
+
+        [Test]
+        public void Given_EmptyOrderAndValidCollection_When_ResolvingSettings_Then_ThrowArgumentNullException()
+        {
+            Sorter<string, int> target = new Sorter<string, int>();
+
+            var locationNames = new List<(string mode, string value)>()
+            {
+                ("Country","United States of America"),
+                ("City","Las Vegas"),
+                ("State","Nevada")
+            };
+
+            Assert.Throws<ArgumentNullException>(() => target.Resolve<(string, int)>(null));
+        }
+
+        private Sorter<string, int> buildTarget()
+        {
+            Sorter<string, int> target = new Sorter<string, int>();
+
+            target.Order.Add("City", 100);
+            target.Order.Add("State", 200);
+            target.Order.Add("Country", 300);
+            target.Order.Add("Default", int.MaxValue);
+
+            return target;
+        }
+    }
+}
---
+++ PeckingOrder/Sorter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PeckingOrder.Collections;
+
+namespace PeckingOrder
+{
+    public class Sorter<TMode, TModeRank> where TModeRank : IComparable
+    {
+        public BiDictionary<TMode, TModeRank> Order { get; set; } = new BiDictionary<TMode, TModeRank>();
+
+        public TValue Resolve<TValue>(IEnumerable<(TMode Mode, TValue Value)> settings)
+        {
+            return ResolveContainer(settings).Value;
+        }
+
+        public (TMode Mode, TValue Value) ResolveContainer<TValue>(IEnumerable<(TMode Mode, TValue Value)> settings)
+        {
+            validate(settings);
+
+            return settings.Aggregate((selected, x) =>
+            {
+                if (Order[x.Mode].CompareTo(Order[selected.Mode]) < 0)
+                {
+                    return x;
+                }
+
+                return selected;
+            });
+        }
+
+        private void validate<TValue>(IEnumerable<(TMode Mode, TValue Value)> settings)
+        {
+            if (Order == null)
+            {
+                throw new InvalidOperationException("Cannot resolve settings while Order property is null.");
+            }
+
+            if (settings == null)
+            {
+                throw new ArgumentNullException("Settings cannot be resolved because the settings parameter is null.");
+            }
+
+            if(!settings.Any())
+            {
+                throw new ArgumentException("Settings cannot be resolved because the settings collection is empty.");
+            }
+
+            foreach (var setting in settings)
+            {
+                if (!Order.ContainsKey(setting.Mode))
+                {
+                    throw new ArgumentOutOfRangeException($"Pecking Order cannot evaluate {setting.Mode.ToString()} because it has not been added to the Sorter's Order property.");
+                }
+            }
+        }
+    }
+}
